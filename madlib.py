00ae4173a4bb7213671ffac177f27b1993ef4540
MaxMcQueen <108023560+MaxMcQueen@users.noreply.github.com>
2023-04-15 13:09:55 -0700

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,10 @@
+noun = input ("Noun: ") #string variable
+#print ("you found a wild " + noun)
+#print ("you found a wild {}".format(noun))
+#print (F"you found a wild {noun}")
+adj = input("Adjective: ")
+action = input ("Action verb ")
+VIP = input ("Famous Person: ")
+madlib = f"that {noun} tasted so {adj} when deep fried with sprinkles on top. It's even better while I'm {action} on mount everst with {VIP}"
+
+print (madlib)
